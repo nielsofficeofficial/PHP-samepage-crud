f9ec4151661510e211cbc87cefc0233d265ad2d9
nielsoffice <nieldigitalsolution@gmail.com>
2021-03-23 15:33:19 +0800

    PHP-samepage-crud

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# PHP-samepage-crud
-PHP-samepage-crud generate w/PHPHtml-Optimizer
+# PHP-samepage-crud generated with PHPHtml-optimizer v1.3.3
+
+
+<h6>Support PHP v7.4.9++ higher | not work v.7 less</h6>
+<p>PHP CRUD on same page effortless </p> 
+
+```PHP
+ // Create database for CRUD run with SQL environment
+ 
+```
+
+<h2>Thanks To:</h2>
+<h5>
+Github : To allow me to upload my PHPHtml-Optimizer PHP Library to repository<br /> 
+php.net : To oppurtunity Develop web application using corePHP - PHPFrameworks<br />
+</h5>
+
+__Please Kindly read license and agreement GNU v3.0__
+<br /> Thanks and good luck! 
+
---
+++ db.php
@@ -0,0 +1,47 @@
+<?php 
+
+/**
+ * @copyright (c) 2020-2021 PHP CRUD SAME PAGE v1.0 Cooked by nielsoffice
+ *
+ * PHP CRUD SAME PAGE v1.0 free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of 
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * @category  PHP CRUD ON SAME PAGE
+ * @package   - corePHP ( purePHP / no html for Back End PHP DEVELOPER/PROGRAMMER)
+ *            - Read License and enjoy!
+ *            
+ * @author    nielfernandez <nieldigitalsolution@gmail.com>
+ * @license   http://_source.org/licenses/gpl-3.0.html GNU Public License
+ * @link      https://github.com/nielsofficeofficial/PHP-samepage-crud
+ * @link      https://github.com/nielsofficeofficial/PHP-samepage-crud/blob/main/README.md
+ * @link      https://www.facebook.com/nielsofficeofficial
+ * @version   v1.0
+ *
+ */
+
+/**
+ *
+ * Defined Database Connection
+ * @since 03.19.21
+ * @since v1.0
+ *
+**/
+
+// server host provider
+define('SERVER_HOST_NAME' ,'localhost'); 
+// db_username
+define('SERVER_USER_NAME' ,'root');   
+// db_password   
+define('SERVER_PASSWORD'  ,'');   
+// db_table       
+define('SERVER_DB_TABLE'  ,'wp1');    
