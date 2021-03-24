935f98c6c688d9c2ef2fd7f4b2b1e1d1ba51d7a5
nielsoffice <67406593+nielsofficeofficial@users.noreply.github.com>
2021-03-24 14:16:39 +0800

    Update PHP-samepage-crud.php

--- PHP-samepage-crud.php
+++ PHP-samepage-crud.php
@@ -115,7 +115,7 @@ require __DIR__ . '/functions.php';
 
     _xdiv(
       ELEM('label','Name: ')
-     .__HTML('Email','input'
+     .__HTML('Name','input'
      ,setEA(['type','name','value'],['text','user_name',"{$unname}"]),'user_name',null 
      
      ,FUNC_ASSOC)
@@ -129,14 +129,14 @@ require __DIR__ . '/functions.php';
      );
     _xdiv(
      ELEM('label','Contact: ')
-    .__HTML('Email','input'
+    .__HTML('Contact','input'
     ,setEA(['type','name','value'],['text','user_contact',"{$ucontact}"]),'user_contact',null 
     
     ,FUNC_ASSOC)
     );
     _xdiv(
       ELEM('label','Social Media: ')
-     .__HTML('Email','input'
+     .__HTML('Social Media','input'
      ,setEA(['type','name','value'],['text','user_sm',"{$usocialm}"]),'user_sm',null 
      
      ,FUNC_ASSOC)
